af25b88321cdce2e50823462edc70d56789d72e5
jujulili1 <tyulianto0795@gmail.com>
2020-06-26 14:47:32 +0700

    first commit

---
+++ script/company.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'You Applied For This Job!',
+        showConfirmButton: false,
+        timer: 1500
+    })
+}
