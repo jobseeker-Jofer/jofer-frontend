13f75168b09c365abc7656d722b786c86657f04c
jujulili1 <tyulianto0795@gmail.com>
2020-06-26 16:43:58 +0700

    update js login

--- script/login-regis.js
+++ script/login-regis.js
@@ -1,4 +1,26 @@
 function toggleForm() {
-    let container = document.querySelector('.container');
+    var container = document.querySelector('.container');
     container.classList.toggle('active')
+}
+
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if (username == "geral" && password == "123") {
+        alert("Login successfully");
+        window.location = "./dashboard.html"; // Redirecting to other page.
+        return false;
+    } else {
+        attempt--; // Decrementing by one.
+        alert("You have left " + attempt + " attempt;");
+        // Disabling fields after 3 attempts.
+        if (attempt == 0) {
+            document.getElementById("username").disabled = true;
+            document.getElementById("password").disabled = true;
+            document.getElementById("submit").disabled = true;
+            return false;
+        }
+    }
 }
