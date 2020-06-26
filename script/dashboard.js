9ff2def3619b080d1c92e9339721e78f8c13329b
Resha Puspita <reshapuspita734@gmail.com>
2020-06-26 15:44:33 +0700

    add backend

---
+++ script/dashboard.js
@@ -0,0 +1,44 @@
+// --------------- ELEMENTS ----------------
+let name = document.getElementById("name");
+let title = document.getElementById("title");
+let desc = document.getElementById("desc");
+let contact = document.getElementById("contact");
+let birthday = document.getElementById("birthday");
+let residence = document.getElementById("residence");
+let skills = document.getElementById("skills");
+
+// --------------- FETCH ----------------
+let endpoint = "https://5ef168ca1faf160016b4d5b5.mockapi.io/api/users/1";
+let options = {
+  method: "GET",
+};
+
+async function getProfile() {
+  try {
+    let response = await fetch(endpoint, options);
+    let results = await response.json();
+    console.log(results);
+    // profile
+    name.innerHTML = `${results.name}`;
+    title.innerHTML = `${results.title}`;
+    desc.innerHTML = `${results.desc}`;
+    contact.innerHTML = `${results.contact}`;
+    birthday.innerHTML = `${results.birthday}`;
+    residence.innerHTML = `${results.city}, Indonesia`;
+    email.innerHTML = `${results.email}`;
+
+    let skillsArr = results.skills;
+    skillsArr.forEach((skill) => {
+      const li = document.createElement("li");
+      const text = document.createTextNode(skill);
+
+      li.appendChild(text);
+
+      skills.appendChild(li);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getProfile();
