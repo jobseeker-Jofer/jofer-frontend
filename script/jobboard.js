c48ef6101cbd5010a07f1b820347395cc0745e1c
Resha Puspita <reshapuspita734@gmail.com>
2020-06-27 01:43:01 +0700

    fitur search works

--- script/jobboard.js
+++ script/jobboard.js
@@ -4,9 +4,9 @@ let ul = document.querySelector(".list-unstyled");
 // let node = "/img/node-list.png";
 // let ibm = "/img/ibm-list.png";
 let images = [
-  "/img/ibm-list.png",
-  "/img/zenius-list.png",
-  "/img/node-list.png",
+  "/img/logo/ibm-list.png",
+  "/img/logo/zenius-list.png",
+  "/img/logo/node-list.png",
 ];
 // <li class="media">
 //   <img src="/img/zenius-list.png" class="mr-3" alt="...">
@@ -16,10 +16,12 @@ let images = [
 //     <p>c</p>
 //   </div>
 // </li>
+
 // --------------- FETCH ----------------
 // let endpoint = "https://5ef168d71faf160016b4d5c1.mockapi.io/api/todoapp/users";
 let endpoint = "https://5ef168ca1faf160016b4d5b5.mockapi.io/api/jobList";
 
+// ----------- FUCNTION -----------
 async function getJobList() {
   try {
     let response = await fetch(endpoint);
@@ -64,5 +66,31 @@ async function getJobList() {
     console.log(error);
   }
 }
+async function filter() {
+  try {
+    // Declare variables
+    let input, filter, li, a, i, txtValue;
+    input = document.getElementById("search");
+    filter = input.value.toUpperCase();
+    li = ul.getElementsByTagName("li");
 
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+// ----------- EVENT LISTENER -----------
+let searchBar = document.getElementById("search");
+searchBar.addEventListener("keyup", filter);
+
+// ----------- MAIN JOBBOARD -----------
 getJobList();
