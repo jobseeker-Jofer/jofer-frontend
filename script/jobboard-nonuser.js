8ca1dae8cb2325789a5c5095b9dd21d0fc2a935d
Resha Puspita <reshapuspita734@gmail.com>
2020-06-27 08:13:25 +0700

    sync branch resha-staging

--- script/jobboard-nonuser.js
+++ script/jobboard-nonuser.js
@@ -4,9 +4,9 @@ let ul = document.querySelector(".list-unstyled");
 // let node = "/img/node-list.png";
 // let ibm = "/img/ibm-list.png";
 let images = [
-  "/img/ibm-list.png",
-  "/img/zenius-list.png",
-  "/img/node-list.png",
+    "/img/logo/ibm-list",
+    "/img/logo/zenius-list.png",
+    "/img/logo/node-list.png",
 ];
 // <li class="media">
 //   <img src="/img/zenius-list.png" class="mr-3" alt="...">
@@ -21,48 +21,48 @@ let images = [
 let endpoint = "https://5ef168ca1faf160016b4d5b5.mockapi.io/api/jobList";
 
 async function getJobList() {
-  try {
-    let response = await fetch(endpoint);
-    let results = await response.json();
-    console.log(results);
-    // job list
-    results.forEach((job, index) => {
-      let li = document.createElement("li");
-      li.setAttribute("class", "media");
+    try {
+        let response = await fetch(endpoint);
+        let results = await response.json();
+        console.log(results);
+        // job list
+        results.forEach((job, index) => {
+            let li = document.createElement("li");
+            li.setAttribute("class", "media");
 
-      let logoCom = document.createElement("img");
-      logoCom.setAttribute("src", images[index]);
-      logoCom.setAttribute("class", "mr-3");
+            let logoCom = document.createElement("img");
+            logoCom.setAttribute("src", images[index]);
+            logoCom.setAttribute("class", "mr-3");
 
-      let mediaBody = document.createElement("div");
-      mediaBody.setAttribute("class", "media-body");
+            let mediaBody = document.createElement("div");
+            mediaBody.setAttribute("class", "media-body");
 
-      let title = document.createElement("a");
-      let jobTitle = document.createTextNode(`${job.title}`);
-      title.setAttribute("class", "mt-0 mb-1 title");
-      title.setAttribute("href", "#");
-      title.appendChild(jobTitle);
-      let company = document.createElement("p");
-      company.setAttribute("id", `${job.title}`);
-      let companyText = document.createTextNode(`${job.company}`);
-      company.appendChild(companyText);
-      let location = document.createElement("p");
-      location.setAttribute("id", `${job.location}`);
-      let locText = document.createTextNode(`${job.location}`);
-      location.appendChild(locText);
+            let title = document.createElement("a");
+            let jobTitle = document.createTextNode(`${job.title}`);
+            title.setAttribute("class", "mt-0 mb-1 title");
+            title.setAttribute("href", "#");
+            title.appendChild(jobTitle);
+            let company = document.createElement("p");
+            company.setAttribute("id", `${job.title}`);
+            let companyText = document.createTextNode(`${job.company}`);
+            company.appendChild(companyText);
+            let location = document.createElement("p");
+            location.setAttribute("id", `${job.location}`);
+            let locText = document.createTextNode(`${job.location}`);
+            location.appendChild(locText);
 
-      mediaBody.appendChild(title);
-      mediaBody.appendChild(company);
-      mediaBody.appendChild(location);
+            mediaBody.appendChild(title);
+            mediaBody.appendChild(company);
+            mediaBody.appendChild(location);
 
-      li.appendChild(logoCom);
-      li.appendChild(mediaBody);
+            li.appendChild(logoCom);
+            li.appendChild(mediaBody);
 
-      ul.appendChild(li);
-    });
-  } catch (error) {
-    console.log(error);
-  }
+            ul.appendChild(li);
+        });
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 getJobList();
--- script/profile.js
+++ script/profile.js
@@ -3,72 +3,72 @@ let saveBtn = document.getElementById("button");
 
 // --------------- FUNCTION ----------------
 async function saveProfile() {
-  try {
-    let user = JSON.parse(localStorage.getItem("user"));
-    console.log(user);
+    try {
+        let user = JSON.parse(localStorage.getItem("user"));
+        console.log(user);
 
-    let id = user.id;
-    console.log(id);
-    let endpoint = `https://5ef168ca1faf160016b4d5b5.mockapi.io/api/users/${id}`;
-    let response = await fetch(endpoint);
-    let results = await response.json();
-    console.log(results);
+        let id = user.id;
+        console.log(id);
+        let endpoint = `https://5ef168ca1faf160016b4d5b5.mockapi.io/api/users/${id}`;
+        let response = await fetch(endpoint);
+        let results = await response.json();
+        console.log(results);
 
-    let title = document.getElementById("title").value;
-    let desc = document.getElementById("desc").value;
-    let contact = document.getElementById("contact").value;
-    let birthday = document.getElementById("birthday").value;
-    let city = document.getElementById("city").value;
+        let title = document.getElementById("title").value;
+        let desc = document.getElementById("desc").value;
+        let contact = document.getElementById("contact").value;
+        let birthday = document.getElementById("birthday").value;
+        let city = document.getElementById("city").value;
 
-    if (title == "") {
-      title = results.title;
+        if (title == "") {
+            title = results.title;
+        }
+        if (desc == "") {
+            desc = results.desc;
+        }
+        if (contact == "") {
+            contact = results.contact;
+        }
+        if (birthday == "") {
+            birthday = results.birthday;
+        }
+        if (city == "") {
+            city = results.city;
+        }
+
+        let userData = {
+            title,
+            desc,
+            contact,
+            birthday,
+            city,
+        };
+        console.log(userData);
+
+        let options = {
+            method: "PUT",
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify(userData),
+        };
+
+        response = await fetch(endpoint, options);
+        console.log(response.json());
+    } catch (error) {
+        console.error(error);
     }
-    if (desc == "") {
-      desc = results.desc;
-    }
-    if (contact == "") {
-      contact = results.contact;
-    }
-    if (birthday == "") {
-      birthday = results.birthday;
-    }
-    if (city == "") {
-      city = results.city;
-    }
-
-    let userData = {
-      title,
-      desc,
-      contact,
-      birthday,
-      city,
-    };
-    console.log(userData);
-
-    let options = {
-      method: "PUT",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(userData),
-    };
-
-    response = await fetch(endpoint, options);
-    console.log(response.json());
-  } catch (error) {
-    console.error(error);
-  }
 }
 
 // --------------- EVENT LISTENER ----------------
 saveBtn.addEventListener("click", function () {
-  //   Swal.fire({
-  //     position: "top-end",
-  //     icon: "success",
-  //     title: "Your work has been saved",
-  //     showConfirmButton: false,
-  //     timer: 1500,
-  //   });
-  saveProfile();
-  alert("Data Succesfully Saved!");
+    //   Swal.fire({
+    //     position: "top-end",
+    //     icon: "success",
+    //     title: "Your work has been saved",
+    //     showConfirmButton: false,
+    //     timer: 1500,
+    //   });
+    saveProfile();
+    alert("Data Succesfully Saved!");
 });
