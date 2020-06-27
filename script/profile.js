0d77486e0730523396392bf9580b4018b5da23b7
Resha Puspita <reshapuspita734@gmail.com>
2020-06-27 08:03:11 +0700

    fix update user profile

--- script/profile.js
+++ script/profile.js
@@ -4,11 +4,37 @@ let saveBtn = document.getElementById("button");
 // --------------- FUNCTION ----------------
 async function saveProfile() {
   try {
+    let user = JSON.parse(localStorage.getItem("user"));
+    console.log(user);
+
+    let id = user.id;
+    console.log(id);
+    let endpoint = `https://5ef168ca1faf160016b4d5b5.mockapi.io/api/users/${id}`;
+    let response = await fetch(endpoint);
+    let results = await response.json();
+    console.log(results);
+
     let title = document.getElementById("title").value;
     let desc = document.getElementById("desc").value;
     let contact = document.getElementById("contact").value;
     let birthday = document.getElementById("birthday").value;
     let city = document.getElementById("city").value;
+
+    if (title == "") {
+      title = results.title;
+    }
+    if (desc == "") {
+      desc = results.desc;
+    }
+    if (contact == "") {
+      contact = results.contact;
+    }
+    if (birthday == "") {
+      birthday = results.birthday;
+    }
+    if (city == "") {
+      city = results.city;
+    }
 
     let userData = {
       title,
@@ -26,13 +52,8 @@ async function saveProfile() {
       },
       body: JSON.stringify(userData),
     };
-    let user = JSON.parse(localStorage.getItem("user"));
-    console.log(user);
 
-    let id = user.id;
-    console.log(id);
-    let endpoint = `https://5ef168ca1faf160016b4d5b5.mockapi.io/api/users/${id}`;
-    let response = await fetch(endpoint, options);
+    response = await fetch(endpoint, options);
     console.log(response.json());
   } catch (error) {
     console.error(error);
