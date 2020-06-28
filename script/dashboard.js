07b0bd3f7b081776949ded1cb0448ff9447e4b0a
Resha Puspita <reshapuspita734@gmail.com>
2020-06-28 12:17:04 +0700

    add email di profile user

--- script/dashboard.js
+++ script/dashboard.js
@@ -3,44 +3,39 @@ let name = document.getElementById("name");
 let title = document.getElementById("title");
 let desc = document.getElementById("desc");
 let contact = document.getElementById("contact");
+let email = document.getElementById("email");
 let birthday = document.getElementById("birthday");
 let residence = document.getElementById("residence");
 let skills = document.getElementById("skills");
 
 // --------------- FUNCTION ----------------
 async function getProfile() {
-  try {
-    let user = JSON.parse(localStorage.getItem("user"));
-    console.log(user);
+    try {
+        let user = JSON.parse(localStorage.getItem("user"));
+        console.log(user);
 
-    let id = user.id;
-    console.log(id);
-    let endpoint = `https://5ef168ca1faf160016b4d5b5.mockapi.io/api/users/${id}`;
+        let id = user.id;
+        console.log(id);
+        let endpoint = `https://5ef168ca1faf160016b4d5b5.mockapi.io/api/users/${id}`;
 
-    let response = await fetch(endpoint);
-    let results = await response.json();
-    console.log(results);
-    // profile
-    name.innerHTML = `${results.name}`;
-    title.innerHTML = `${results.title}`;
-    desc.innerHTML = `${results.desc}`;
-    contact.innerHTML = `${results.contact}`;
-    birthday.innerHTML = `${results.birthday}`;
-    residence.innerHTML = `${results.city}, Indonesia`;
-    email.innerHTML = `${results.email}`;
+        let response = await fetch(endpoint);
+        let results = await response.json();
+        console.log(results);
+        // profile
+        name.innerHTML = `${results.name}`;
+        title.innerHTML = `${results.title}`;
+        desc.innerHTML = `${results.desc}`;
+        contact.innerHTML = `${results.contact}`;
+        birthday.innerHTML = `${results.birthday}`;
+        residence.innerHTML = `${results.city}, Indonesia`;
+        email.innerHTML = `${results.email}`;
 
-    let skillsArr = results.skills;
-    skillsArr.forEach((skill) => {
-      const li = document.createElement("li");
-      const text = document.createTextNode(skill);
-
-      li.appendChild(text);
-
-      skills.appendChild(li);
-    });
-  } catch (error) {
-    console.log(error);
-  }
+        console.log("skills:", results.skills);
+        let skillsArr = results.skills;
+        skills.innerHTML = skillsArr.join(", ");
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 getProfile();
